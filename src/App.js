2879631413d6539feab4063d57b8a405de7f327e
Jennifer Perez <jenniferperez@pursuit.org>
2022-01-06 19:52:51 -0600

    wip react route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import Navbar from "./components/navbar.js";
+import MainPage from "./components/mainPage.js";
 import "./styles/app.css";
 // import {Route,  Switch} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Routes, // instead of "Switch"
+//   Route,
+// } from "react-router-dom";
+
 
 class App extends Component{
 
@@ -12,11 +19,16 @@ class App extends Component{
   render(){
     return (
       <>
-    <div className="App">
-      <header className="App-header">  
-      <Navbar/>
-      </header>
-    </div>
+    {/* <div className="App"> */}
+      {/* <header className="App-header">   */}
+      {/* <browserRouter>   */}
+      {/* <Route path="/navbar" exact strict component={Navbar}/>
+      <Route path="/mainPage" exact strict component={MainPage}/> */}
+       <Navbar/>
+      {/*<MainPage/> */}
+      {/* </browserRouter> */}
+      {/* </header> */}
+    {/* </div> */}
       </>
     )
   }
---
+++ src/components/mainPage.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";;
+
+class MainPage extends Component{
+  state={
+
+  }
+  render(){
+    return(
+      <>
+   <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <p>Content content content content</p>
+          </div>
+        </div>
+      </div>
+      </>
+       
+     
+
+     
+    )
+  }
+}
+export default MainPage;
