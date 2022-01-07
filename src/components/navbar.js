9f4171c66304cf4c65343154e902e0be810d9292
Jennifer Perez <jenniferperez@pursuit.org>
2022-01-06 19:11:26 -0600

    added spacing in between each nav item

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,9 +22,9 @@ const Navbar = () => {
             <span className="navbar-toggler-icon "></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#">
+                <a className="nav-link mx-2" aria-current="page" href="#">
                   about
                 </a>
               </li>
