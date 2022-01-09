0fe42559b4d08087b656234158d8fa175e96b24a
Jennifer Perez <jenniferperez@pursuit.org>
2022-01-09 16:13:53 -0600

    added cards to the review Section

--- src/components/reviewsSection.js
+++ src/components/reviewsSection.js
@@ -11,19 +11,28 @@ class ReviewsSection extends Component {
     return (<> 
     <div class="container">
     <div class="row align-items-start">
+   
         {
           reviews.map((review,i)=>{
            return ( 
-           
-           <div let key={i} class="col col-4 p-2">
-            {review.review} 
+            <div let key={i} class="col col-4 p-2">
+            <div  class="card">
+
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>{review.review}</p>
+                <footer class="blockquote-footer">{review.name}</footer>
+              </blockquote>
             </div>
+          </div> 
+          </div>
               
             )
           })
         }
     </div>
     </div>
+  
     </>)
   }
 }
