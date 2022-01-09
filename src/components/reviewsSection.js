591ee42327410433744f2fffa318b9068d2b6430
Jennifer Perez <jenniferperez@pursuit.org>
2022-01-09 14:05:01 -0600

    properly displayed reviews wip

--- src/components/reviewsSection.js
+++ src/components/reviewsSection.js
@@ -5,28 +5,26 @@ class ReviewsSection extends Component {
  constructor(props) {
    super(props) 
       this.state={
-
       }
-   
  }
-
   render(){
-    return(<>
-  <div class="container">  
-        <div class="row align-items-start">
-      {
-      reviews.map((review,i)=>{
-        
-        return <div let key={i} class="col m-2">
-        {review.review} </div>
-
-      })
-    }
-         </div>
-        </div>
-
+    return (<> 
+    <div class="container">
+    <div class="row align-items-start">
+        {
+          reviews.map((review,i)=>{
+           return ( 
+           
+           <div let key={i} class="col col-4 p-2">
+            {review.review} 
+            </div>
+              
+            )
+          })
+        }
+    </div>
+    </div>
     </>)
   }
 }
-
 export default ReviewsSection;
